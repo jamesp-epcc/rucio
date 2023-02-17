2c3dd274b695907f83a309b66bc47e4401236b28
James Perry <j.perry@epcc.ed.ac.uk>
2023-02-17 11:36:09 +0000

    Add DUNE's metacat plugin to Rucio core

---
+++ lib/rucio/core/did_meta_plugins/metacat_meta.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# Copyright European Organization for Nuclear Research (CERN) since 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import TYPE_CHECKING
+
+from metacat.webapi import MetaCatClient
+
+from rucio.core.did_meta_plugins.did_meta_plugin_interface import DidMetaPlugin
+
+class MetaCatRucioPlugin(DidMetaPlugin):
+    
+    def __init__(self, client):
+        super(MetaCatRucioPlugin, self).__init__()
+        # FIXME: initialise this properly
+        self.Client = client
+        self.plugin_name = "METACAT"
+        
+    def get_metadata(self, scope, name, *, session: "Optional[Session]" = None):
+        info = self.Cient.get_file_info(name=f"{scope}:{name}")
+        return info.Metadata
+
+    def set_metadata(self, scope, name, key, value, recursive=False, *,
+                     session: "Optional[Session]" = None):
+        self.Client.update_file_meta(
+            {key:value},
+            names=[f"{scope}:{name}"]
+        )
+
+    def set_metadata_bulk(self, scope, name, meta, recursive=False, *,
+                          session: "Optional[Session]" = None):
+        self.Client.update_file_meta(
+            meta,
+            names=[f"{scope}:{name}"]
+        )
+
+    def delete_metadata(self, scope, name, key, *,
+                        session: "Optional[Session]" = None):
+        meta = self.get_metadata(scope, name)
+        try:    del meta[key]
+        except KeyError:    return
+        self.Client.update_file_meta(
+            meta,
+            names=[f"{scope}:{name}"],
+            mode="replace"
+        )
+
+    def list_dids(self, scope, filters, did_type='collection', ignore_case=False,
+                  limit=None, offset=None, long=False, recursive=False,
+                  ignore_dids=None, *, session: "Optional[Session]" = None):
+        # FIXME: implement ignore_dids!
+        where_items = []
+        for k, v in filters.items():
+            if isinstance(v, str):
+                where_items.append(f"{k} = '{v}'")
+            else:
+                where_items.append(f"{k} = {v}")
+        where_clause = " and ".join(where_items)
+                  
+        if did_type in ("collection", "dataset", "container"):
+            if where_clause:    where_clause = " having "+where_clouse
+            query = f"datasets {scope}:'%' {where_clause}"
+            if recursive:
+                query += " with children recursively"
+        else:
+            if where_clause:    where_clause = " where "+where_clouse
+            query = f"files from {scope}:'%'"
+            if recursive:
+                query += " with children recursively"
+            query += where_clouse
+        
+        if limit is not None:
+            query += f" limit {limit}"
+            
+        results = self.Client.run_query(query)
+        return [item["name"] for item in results]
+                  
+    def manages_key(self, key, *, session: "Optional[Session]" = None):
+        return True
+
+    def get_plugin_name(self):
+        return self.plugin_name
+
